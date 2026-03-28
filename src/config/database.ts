a43f7028cbac10b1c2cfbd2eb1a350b5f110c632
Emon Hossain <islamemon59@gmail.com>
2026-03-28 11:25:55 +0600

    auth base table refactored

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,32 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+import { env } from './env';
+
+type GlobalPrismaState = {
+  prisma?: PrismaClient;
+  pgPool?: Pool;
+};
+
+const globalForPrisma = globalThis as unknown as GlobalPrismaState;
+
+const pgPool =
+  globalForPrisma.pgPool ||
+  new Pool({
+    connectionString: env.DATABASE_URL,
+  });
+
+const adapter = new PrismaPg(pgPool);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ['query', 'error', 'warn'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.pgPool = pgPool;
 }
