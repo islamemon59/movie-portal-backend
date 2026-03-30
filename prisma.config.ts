03528a4df26e2fb2f7ecea190955640545f999ca
Emon Hossain <islamemon59@gmail.com>
2026-03-30 23:15:12 +0600

    prisma config file updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema',
-  engine: 'classic',
   datasource: {
     url: env('DATABASE_URL'),
   },
